b8f71da6485762fd575fcd9ca9f368f8205083cc
Andrea <b.a.benvenuti3@gmail.com>
2021-03-24 16:17:30 +0100

    Aggiunti 'mouseOver' e 'mouseLeave' in methods

--- js/script.js
+++ js/script.js
@@ -5,8 +5,23 @@ var app = new Vue(
       message: "Vue.js",
       img: "https://vuejs.org/images/logo.png",
       link: "https://vuejs.org/",
+      color: "primary_color"
     },
     methods: {
+      mouseOver: function(){
+        if (this.color == "primary_color") {
+          this.color = "secondary_color";
+        } else{
+          this.color = "primary_color";
+        }
+      },
+      mouseLeave: function(){
+        if (this.color == "secondary_color") {
+          this.color = "primary_color";
+        } else{
+          this.color = "secondary_color";
+        }
+      }
     }
   }
 );
