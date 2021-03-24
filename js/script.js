54b6fbc7868bc772688552da9c7156b10e6353f2
Andrea <b.a.benvenuti3@gmail.com>
2021-03-24 15:47:30 +0100

    Completata richiesta esercizio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var app = new Vue(
+  {
+    el: '#root',
+    data: {
+      message: "Vue.js",
+      img: "https://vuejs.org/images/logo.png",
+      link: "https://vuejs.org/",
+    },
+    methods: {
+    }
+  }
+);
